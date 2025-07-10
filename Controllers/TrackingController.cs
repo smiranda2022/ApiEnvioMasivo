75f446f65d5d7d57555d218d05cf9237b1ca5178
santiago miranda <santiago.g.miranda@gmail.com>
2025-07-10 08:12:58 -0300

    Actualización en TrackingController.cs

--- Controllers/TrackingController.cs
+++ Controllers/TrackingController.cs
@@ -19,28 +19,44 @@ namespace ApiEnvioMasivo.Controllers
         }
 
         [HttpGet("open")]
+
         public async Task<IActionResult> RegistrarApertura([FromQuery] int correoId)
         {
-            //var registro = await _db.CorreosEnviados.FindAsync(correoId);
-            var registro = await _db.CorreosEnviados.FirstOrDefaultAsync(x => x.DestinatarioId == correoId);
+            var registro = await _db.CorreosEnviados
+                .FirstOrDefaultAsync(x => x.Id == correoId);
+
             if (registro != null && !registro.Abierto)
             {
                 registro.Abierto = true;
                 registro.FechaApertura = DateTime.UtcNow;
+
+                // Actualizar FlujoHistorial solo por DestinatarioId
+                var historial = await _db.FlujoHistoriales.FirstOrDefaultAsync(h =>
+                    h.DestinatarioId == registro.DestinatarioId
+                );
+
+                if (historial != null)
+                {
+                    historial.Abierto = true;
+                }
+
                 await _db.SaveChangesAsync();
             }
 
-            // Devolver una imagen 1x1 vacía (pixel transparente)
+            // Pixel 1x1 transparente
             var pixel = new byte[]
             {
-                71,73,70,56,57,97,1,0,
-                1,0,128,0,0,255,255,255,
-                0,0,0,33,249,4,1,0,0,1,
-                0,44,0,0,0,0,1,0,1,0,
-                0,2,2,76,1,0,59
+        71,73,70,56,57,97,1,0,
+        1,0,128,0,0,255,255,255,
+        0,0,0,33,249,4,1,0,0,
+        0,0,44,0,0,0,0,1,0,
+        1,0,2,2,76,1,0,59
             };
 
             return File(pixel, "image/gif");
         }
+
+
+
     }
 }
